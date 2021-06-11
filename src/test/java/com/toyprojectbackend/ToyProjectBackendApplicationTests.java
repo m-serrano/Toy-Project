bd3226c763f0aea3585a247607650821e40b75ae
Administrator <slinux.ve2@gmail.com>
2021-06-11 09:40:06 -0500

    Sprint initializr elements

---
+++ src/test/java/com/toyprojectbackend/ToyProjectBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.toyprojectbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToyProjectBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
